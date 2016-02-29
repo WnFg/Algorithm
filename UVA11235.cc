4106d08b1eeb0df6de1ef515231e62d0c06440aa
root <root@TENCENT.site>
2016-02-29 12:44:35 +0800

    10

--- UVA11235.cc
+++ UVA11235.cc
@@ -23,10 +23,9 @@ void init(int n)
 		table[i][0] = total[i] - a;
 		a = total[i];
 	}
-	int limit = log2(n);
 //	cout << limit << endl;
 	int tt = 1;
-	for(int j = 1; j <= limit; j++){
+	for(int j = 1; (1<<j) <= n; j++){
 		for(int i = 0; i + (tt << 1) - 1 < n; i++){
 			table[i][j] = max(table[i][j - 1], table[i + tt][j - 1]);
 		}
@@ -43,7 +42,7 @@ int query(int l, int r)
 	if(record[r] != 0)
 		right =  r + 1 - total[record[r] - 1];
 	else
-		right = r;
+		right = r + 1;
 	int ans = max(left, right);
 	r = record[r] - 1;
 	l = record[l] + 1;
@@ -51,7 +50,8 @@ int query(int l, int r)
 	if(r - l < 0)
 		return ans;
 	
-	int len = log2(r - l + 1);
+	int len = 0;
+	while( 1 << (len + 1) <= r - l + 1)  len++;
 	int t = 1 << len;
 	return max(ans, max(table[l][len], table[r - t + 1][len]));
 }
@@ -60,14 +60,17 @@ int main()
 	int n, q;
 	while(scanf("%d", &n), n != 0){
 	//	cout << log2(n) << endl;
+		memset(total, 0, sizeof(total));
 		scanf("%d", &q);
-		int t = -1, last = -11111111;
+		int t = -1, last = -9999999;
 		int a;
 		for(int i = 0; i < n; i++){
 			scanf("%d", &a);
 			if(a != last){
-				if(t > -1)
-					total[++t] = 1 + total[t - 1];
+				if(t > -1){
+					total[t + 1] = 1 + total[t];
+					++t;
+				}
 				else
 					total[++t] = 1;
 			}else
@@ -79,6 +82,7 @@ int main()
 		int l, r;
 		while(q--){
 			scanf("%d %d", &l, &r);
+			if(l > r) swap(l, r);
 			printf("%d\n", query(l - 1, r - 1));
 		}
 	}
